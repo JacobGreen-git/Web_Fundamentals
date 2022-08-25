9339c549f6249daa396644edbd19797ac2668b2f
JacobGreen-git <jacoblgreen96@gmail.com>
2022-08-25 10:22:31 -0500

    Algo

---
+++ 00_algos/wk2_d1.js
@@ -0,0 +1,11 @@
+// console.log(Math.floor(3.9)); //Will always round down
+// console.log(Math.ceil(3.01)); //Will always round up
+
+// console.log(Math.random()) //Gives a random number between 0-1 (Including decimals)
+
+// console.log(Math.random()*20) //Gives a random number between 0-20
+
+// console.log((Math.random()*20)+ 10) //gives a random number between 10-30
+
+
+console.log(Math.floor(Math.random()*2)) //will give you a whole number between 0-2 (but not 2)
